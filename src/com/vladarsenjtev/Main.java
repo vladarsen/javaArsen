13c428bbe7a23e917efc621a23f1ab1ae400b269
Vlad Arsenjtev <vlad.arsenjtev@gmail.com>
2021-11-20 17:34:21 +0200

    Add Task 2

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -1,29 +1,25 @@
 package com.vladarsenjtev;
 
 /*
-1) Электронные часы показывают время в формате от 00:00 до 23:59.
-Подсчитать сколько раз за сутки случается так, что слева от двоеточия показывается
-симметричная комбинация для той, что справа от двоеточия (например, 02:20, 11:11 или 15:51)
+2) Найти количество счастливых билетиков на трамвай от 000001 до 999999 -
+те у которых сумма первых 3 цифр равна сумме последних 3
 */
 
-
 public class Main {
-
-
     public static void main(String[] args) {
         int count = 0;
-        for (int hours = 0; hours < 24; hours++) {
-            for (int minutes = 0; minutes < 60; minutes++) {
-                String time2 = String.format("%02d", hours);
-                String time3 = String.format("%02d", minutes);
-                String time4 = new StringBuilder(time3).reverse().toString();
-                if (time2.equals(time4)) {
-                    count++;
-                    System.out.println(time2 + ":" + time3);
-                }
+        for (int i = 0; i <= 999999; i++) {
+            String number = String.format("%06d", i);
+            int sum1 = 0;
+            int sum2 = 0;
+            for (int j = 0; j < 3; j++) {
+                sum1 += number.charAt(j);
+                sum2 += number.charAt(j + 3);
+            }
+            if (sum1 == sum2) {
+                count++;
             }
         }
-        System.out.println("Количество симметричных комбинаций раз за сутки: " + count);
+        System.out.println("Количество счасливых билетов: " + count);
     }
 }
-
